aab1eb0abf66477739687702418103f7a331279c
naname050505 <naname84j@gmail.com>
2018-08-30 08:08:45 +0000

    fix some codes

--- install.sh
+++ install.sh
@@ -7,6 +7,20 @@ apt-get -y install tig
 apt-get -y install htop
 apt-get -y install python
 apt-get -y install curl
+apt-get -y install wget
+
+#4add-apt-rwpository 
+apt-get -y install apt-file
+apt-file -y update
+apt-file -y search add-apt-repository
+apt-get -y install software-properties-common
+
+#4python3
 curl -kL https://bootstrap.pypa.io/get-pip.py |  python3 #install pip
+add-apt-repository -y ppa:jonathonf/python-3.6
+apt-get -y update
+apt-get -y install python3.6 python3.6-dev
+wget https://bootstrap.pypa.io/get-pip.py
+python3.6 get-pip.py
 pip install --upgrade --ignore-installed tensorflow-gpu==1.4
 pip install keras
