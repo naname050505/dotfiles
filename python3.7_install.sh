90e9e27c6a18048072c6f53bc7b22bf9ac1c018b
na_name <naname84j@gmail.com>
2020-08-30 21:10:13 +0900

    Update python3.7_install.sh

--- python3.7_install.sh
+++ python3.7_install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+sudo apt -yV install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.7
+
 curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
 tar xvfz Python-3.7.0.tgz
 cd Python-3.7.0/
