ebc0c28d993918e09632bb0c012cfca23a14a177
na_name <naname84j@gmail.com>
2020-08-30 23:04:54 +0900

    Update python3.7_install.sh

--- python3.7_install.sh
+++ python3.7_install.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
-sudo apt -yV install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install python3.7
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+sudo apt-get install python3-dev
 
-curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
-tar xvfz Python-3.7.0.tgz
-cd Python-3.7.0/
-./configure --prefix=/usr/local/python370 --with-ensurepip
-make
-sudo make install
+
+
+#sudo apt -yV install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt-get update
+#sudo apt-get install python3.7
+
+#curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
+#tar xvfz Python-3.7.0.tgz
+#cd Python-3.7.0/
+#./configure --prefix=/usr/local/python370 --with-ensurepip
+#make
+#sudo make install
